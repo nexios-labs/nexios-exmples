deb1ef23ef98c58deb0da56dc113032cd9bf052b
dunamix <techwithdunamix@gmail.com>
2025-03-14 13:40:52 +0100

    Add WebSocket authentication and chat handling; implement user connection management

--- nexios-chattr/backend/app.py
+++ nexios-chattr/backend/app.py
@@ -1,11 +1,12 @@
 from nexios import get_application,NexiosApp
 from nexios.auth.middleware import AuthenticationMiddleware
 from nexios.auth.backends import JWTAuthBackend
+from nexios.websockets.channels import ChannelBox
 from config import test_config,db_config
 from api.routers import api_router
 from tortoise import Tortoise as db
-from common.get_user import get_user
 from ws import WebsocketRouter
+from common.get_user import get_user
 app = get_application(
     config=test_config
 )
@@ -25,6 +26,8 @@ async def init_db():
 
 @app.on_shutdown
 async def close_db():
+    for key, val in ChannelBox.CHANNEL_GROUPS.items():
+        await val.websocket.close()
     await db.close_connections()
     print("Database closed")
 app.mount_router(api_router)
--- nexios-chattr/backend/ws/chat_handler.py
+++ nexios-chattr/backend/ws/chat_handler.py
@@ -1,19 +1,28 @@
 from nexios.websockets.consumers import WebSocketEndpoint
+from nexios.websockets.base import WebSocket
 from nexios import routing
-
+import typing
+from .services.auth_ws import authenticate_user,WebSocketException
 
 chat_router = routing.WSRouter(prefix="/chat")
 class ChatHandler(WebSocketEndpoint):
+    encoding = "json"
     
-    async def on_connect(self, websocket):
+
+    async def on_connect(self, websocket: WebSocket) -> None:
+        await self.join_group("default")
         await super().on_connect(websocket)
-        self.join_group("user_chats")
+
+    async def on_receive(self, websocket: WebSocket, data: typing.Any) -> None:
+        await self.broadcast(data, group_name="defaults", save_history=True)
+ 
         
         
         
 
 chat_router.add_ws_route(
     route=routing.WebsocketRoutes(
+        
         path="/{user_id}",
         handler=ChatHandler(
             logging_enabled=True
